ca9434a40f10f4c27c9b5700aeb2ae4da6628c8a
JinFong <Jin.Fong@illinois.gov>
2017-05-10 15:52:04 -0500

    added more functions

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -16,6 +16,7 @@
     </br>
     <input type = "text" v-on:keyup.enter ="pressedEnter">
 
+    <hr>
     <h1>Moives</h1>
     <ul>
       <li v-for = "title in movieTitles">{{title}}</li>
@@ -33,7 +34,11 @@
     <p> The Movie is {{isMovieOld(releaseYear) ? 'old' : 'new'}}</p>
     <div v-bind:title="movieTitle">Hover your mouse here to see movie title!</div>
 
+    <hr>
     <h1>Employees</h1>
+    <div v-for = "(index, employee) in employees">{{employee}} : {{index}}
+    </div>
+    </br>
     <table border="1">
     <thead>
       <tr>
@@ -51,6 +56,7 @@
     </tbody>
     </table>
 
+    <hr>
     <div v-bind:style="{'margin':'100px 100px','transform':'rotate(3deg)','box-shadow':'0 20px 30px rgba(0,0,0,1)','background-color':'red'}"> CSS Style Testing
     </div>
 
@@ -60,6 +66,33 @@
         <button @click = "isB = !isB">isB = {{isB}}</button>
       </div>
     </div>
+    </br>
+    </br>
+
+    <hr>
+    <ol>
+      <li v-for = "item in items">{{item.msg}}</li>
+    </ol>
+    <button v-on:click="f1">f1 filter /Foo/</button>
+    <button v-on:click="f2">f2 concat arr</button>
+    <button v-on:click="f3">f3 splice(2,3)</button>
+    <button v-on:click="f4">f4 other functions</button>
+<!--
+    <button v-on:click="f5(item)">this.items.splice(index, 1)</button>
+    <button v-on:click="f6(item)">f6 remove items</button>
+
+        <template v-for = "uid in uids" track-by = "$index">
+            <li>{{$index}}.{{uid._uid}}</li>
+        </template>
+
+        <div v-for = "value in user">{{$key}} : {{value}}
+        </div>
+
+        <div v-for = "(key, val) in user">{{val}} : {{key}}
+        </div>
+        </br>
+
+-->
 
   </div>
 </template>
@@ -97,31 +130,91 @@
           { mName: 'The Matrix Reloaded', year: 2000 },
           { mName: 'The Matrix Revoultions', year: 2003 }
         ],
-
+        items: [
+          { msg: 'Foo'},
+          { msg: 'Bar'},
+          { msg: 'George'},
+          { msg: 'John'},
+          { msg: 'Tom'},
+          { msg: 'James'},
+          { msg: 'Martin'}
+        ],
+        arr: [
+          { msg: 'array 1'},
+          { msg: 'array 2'},
+          { msg: 'array 1'}
+        ],
+        uids: [
+          { _uid: '88f869d'},
+          { _uid: '7496c10'}
+        ],
 
       }
     },
+
     methods: {
       isMovieOld: function (releaseYear){
         return releaseYear < 2000;
       },
+
       showDialog: function(){
         alert("hello world");
       },
+
       pressedEnter: function(){
         alert("You pressed the Enter Key!");
       },
+
       changeName: function(){
         this.user.firstName = 'Mark',
         this.user.lastName = 'Gondale'
       },
+
       addMovie: function () {
         this.movies.push({
           mName: 'The Fast and the Furious',
           year: 2001
         });
       },
+
+      f1: function() {
+        this.items = this.items.filter(function(item){
+            return item.msg.match(/Foo/)
+        });
+      },
+
+      f2: function() {
+        this.items = this.items.concat(this.arr[0])
+        this.items = this.items.concat(this.arr[1])
+        this.items = this.items.concat(this.arr[2])
+      },
+
+      f3: function() {
+        this.items = this.items.splice(2,3)
+      },
+
+      f4: function() {
+        this.items.pop();
+        this.items.push({msg:'push item'});
+        this.items.shift();
+        this.items.unshift();
+        this.items.sort();
+        this.items.reverse();
+      },
+
+      f5: function(item) {
+         var index = this.items.indexOf(item);
+         if (index !== -1){
+           this.items.splice(index,1);
+         }
+      },
+
+      f6: function(item) {
+        this.items.$remove(item);
+      },
+
     },
+
     computed: {
       getFullName: function() {
         return this.user.firstName + ' '+ this.user.lastName;
